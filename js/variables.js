4c1f4c84182966d3c272deefcd52928872be715c
Ulises-lab <yaftemiranda@gmail.com>
2022-05-18 18:50:38 -0500

    .

--- js/variables.js
+++ js/variables.js
@@ -23,42 +23,6 @@ let stop_left_rigth;
 
 let audio = new Audio('SpaceMusicPack/slow-travel.wav')
 
-/* let teclap =0;
-let inx= 0;
-let px= 100;
-let py =100;
-const imgen = new Image();
-imgen.src = 'images/CamiIzq.png'
-
-function dibujacanvas(){
-    ctx.clearRect(0,0,canvas.width,canvas.height)
-    console.log(ctx.drawImage(imgen,inx*64,10,64,64,px,py,32,32));
-
-    inx++;
-    if(inx>3)
-        inx=0;
-    setTimeout('dibujacanvas()',300);
-}
-
-dibujacanvas()
-
-document.onkeydown=function(event){
-    if(event == null){
-        teclap = windows.event.keyCode;
-    }else{
-        teclap = event.keyCode;
-    }
-    switch (teclap) {
-        case 39:
-            px = px+10;
-        break;          
-        case 37:
-            px = px-10;
-        break;
-    }
-} */
-
-
 
 /*
 Piso aleatorio
